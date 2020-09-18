7ee14a0a2ba842ea684733eab3978f33e2e96411
Ahmad Moussawi <aymoussawi@gmail.com>
2020-09-18 17:43:54 +0300

    add readme

--- 13-fibonacci_numbers/fib_frog/index.js
+++ 13-fibonacci_numbers/fib_frog/index.js
@@ -1,55 +1,49 @@
-function fb(n, cache = { 0: 0, 1: 1 }) {
-    if (!(n in cache)) {
-        cache[n] = fb(n - 1, cache) + fb(n - 2, cache);
+function fbs(n) {
+
+    if (n === 0) return [];
+
+    const arr = [0, 1, 1];
+    let i = 3;
+
+    while (arr[i - 1] + arr[i - 2] <= n) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+        i++;
     }
-    return cache[n];
+
+    return arr.slice(2);
 }
 
-function possibleSteps(n) {
-    let i = 3;
-    let result = [1];
-    while (fb(i) <= n) {
-        result.push(fb(i++));
+function solution(A) {
+    let arr = [1, ...A, 1];
+    let steps = fbs(arr.length);
+    let cost = { [arr.length - 1]: 0 };
+
+    for (let i = arr.length - 2; i >= 0; i--) {
+        if (arr[i] === 0) continue;
+
+        cost[i] = -1;
+
+        for (let j = 0; j < steps.length, i + steps[j] < arr.length; j++) {
+            let target = i + steps[j];
+            if (!(arr[target] === 1 && cost[target] > -1)) continue;
+
+            if (cost[i] === -1 || cost[target] + 1 < cost[i]) {
+                cost[i] = cost[target] + 1;
+            }
+
+            // no need to continue in case of
+            // getting the minimum cost possible
+            if (cost[i] === 1) break;
+        }
     }
-    return result;
+
+    return cost[0];
 }
 
 function print() {
     [...arguments].forEach(arr => {
         console.log(`[${arr.join(', ')}]`);
     })
-}
-
-function solution(A) {
-    let arr = [1, ...A, 1];
-    let steps = possibleSteps(arr.length);
-    let cost = { [arr.length - 1]: 0 };
-
-    for (let i = arr.length - 2; i >= 0; i--) {
-        if (arr[i] === 0) {
-            continue;
-        }
-
-        cost[i] = -1;
-
-        for (var j = 0; j < steps.length, i + steps[j] < arr.length; j++) {
-            var target = i + steps[j];
-            if (arr[target] === 1 && cost[target] > -1) {
-
-                if (cost[i] === -1 || cost[target] + 1 < cost[i]) {
-                    cost[i] = cost[target] + 1;
-                }
-
-                // if we get a minimum cost
-                // no need to continue checking
-                if (cost[i] === 1) {
-                    break;
-                }
-            }
-        }
-    }
-
-    return cost[0];
 }
 
 console.log(solution([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]));
