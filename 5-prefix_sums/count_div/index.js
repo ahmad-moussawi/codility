e7cba0b2823c817b54ad9fe6377f9fd79a042b49
Ahmad Moussawi <aymoussawi@gmail.com>
2020-09-23 19:36:28 +0300

    add prefix sum intro

---
+++ 5-prefix_sums/count_div/index.js
@@ -0,0 +1,5 @@
+function solution(A, B, K) {
+
+}
+
+module.exports.solution = solution;
---
+++ 5-prefix_sums/count_div/index.test.js
@@ -0,0 +1,5 @@
+const { solution } = require('./index');
+
+test('Example tests', () => {
+    expect(solution(6, 11, 2)).toBe(3)
+});
