3d95d416ffedf3ec08d706dc857d9ce08772d0b0
Ahmad Moussawi <aymoussawi@gmail.com>
2021-05-27 22:06:42 +0300

    add optimized answer

--- 7-stacks_and_queues/stone_wall/index.js
+++ 7-stacks_and_queues/stone_wall/index.js
@@ -1,20 +1,8 @@
 function solution(H) {
     let count = 0, stack = [];
 
-
     for (let i = 0; i < H.length; i++) {
-        var prev = H[i - 1] || 0;
         var current = H[i];
-
-        if (current === prev) {
-            continue;
-        }
-
-        // going up
-        if (current > prev) {
-            stack.push(current);
-            continue;
-        }
 
         // going down
         while (stack.length && stack[stack.length - 1] >= current) {
@@ -25,14 +13,12 @@ function solution(H) {
         }
 
         stack.push(current);
-
     }
 
     // add the remaining count in the stack if any
     count += stack.length;
 
     return count;
-
 }
 
 module.exports.solution = solution;
