d15d13c70cf692322924577ae0fe04540c5c371b
Ahmad Moussawi <aymoussawi@gmail.com>
2021-05-27 21:47:16 +0300

    add stone_wall

--- 7-stacks_and_queues/stone_wall/index.js
+++ 7-stacks_and_queues/stone_wall/index.js
@@ -1,4 +1,38 @@
 function solution(H) {
+    let count = 0, stack = [];
+
+
+    for (let i = 0; i < H.length; i++) {
+        var prev = H[i - 1] || 0;
+        var current = H[i];
+
+        if (current === prev) {
+            continue;
+        }
+
+        // going up
+        if (current > prev) {
+            stack.push(current);
+            continue;
+        }
+
+        // going down
+        while (stack.length && stack[stack.length - 1] >= current) {
+            if (stack.pop() === current) {
+                break;
+            }
+            count++;
+        }
+
+        stack.push(current);
+
+    }
+
+    // add the remaining count in the stack if any
+    count += stack.length;
+
+    return count;
+
 }
 
 module.exports.solution = solution;
--- 7-stacks_and_queues/stone_wall/index.test.js
+++ 7-stacks_and_queues/stone_wall/index.test.js
@@ -1,5 +1,44 @@
 const { solution } = require('./index');
 
 test('Basic test', () => {
-    // expect(solution(true)).toBe(true);
+
+    expect(solution([8, 8, 5, 7, 9, 8, 7, 4, 8])).toBe(7);
+
+    expect(solution([1])).toBe(1);
+    expect(solution([10])).toBe(1);
+    expect(solution([1, 2])).toBe(2);
+    expect(solution([1, 2, 3])).toBe(3);
+    expect(solution([1, 2, 1])).toBe(2);
+    expect(solution([1, 1, 1, 1])).toBe(1);
+
+
+    expect(solution([8, 8, 5, 7, 9, 8, 7, 5, 8])).toBe(6)
+    expect(solution([8, 8, 5, 7, 9, 9, 7, 5, 7])).toBe(5)
+    expect(solution([8, 8, 5, 7, 9, 7, 7, 5, 7])).toBe(5)
+    expect(solution([8, 8, 5, 7, 7, 7, 7, 5, 7])).toBe(4)
+    expect(solution([8, 8, 5, 7, 7, 7, 7, 5, 7])).toBe(4)
+    expect(solution([8, 8, 9, 7, 7, 7, 7, 5, 7])).toBe(5)
+    expect(solution([8, 8, 9, 7, 7, 9, 7, 5, 7])).toBe(6)
+    expect(solution([8, 8, 8, 7, 7, 6, 6, 5, 5])).toBe(4)
+    expect(solution([1, 2, 3, 4, 3])).toBe(4)
+    expect(solution([8, 8, 5])).toBe(2)
+    expect(solution([8,])).toBe(1)
+    expect(solution([8, 8])).toBe(1)
+    expect(solution([8, 8, 8, 8, 8])).toBe(1)
+    expect(solution([1000000000])).toBe(1)
+    expect(solution([1000000000, 2])).toBe(2)
+    expect(solution([2, 1000000000, 2])).toBe(2)
+    expect(solution([2, 1000000000, 2, 1000000000])).toBe(3)
+    expect(solution([2, 1000000000, 2, 1000000000, 1000000000])).toBe(3)
+
+
+})
+
+test('ascending then descending', () => {
+    expect(solution([1, 9, 1])).toBe(2)
+    expect(solution([1, 9, 1, 1])).toBe(2)
+    expect(solution([1, 9, 1, 1, 1])).toBe(2)
+    expect(solution([1, 9, 1, 9])).toBe(3)
+    expect(solution([1, 9, 1, 9, 1])).toBe(3)
+
 })
